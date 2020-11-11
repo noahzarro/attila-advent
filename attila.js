df4d87123e1561d346a11a3a05215bbfdbfedd91
Noah Zarro <noahzarro@gmail.com>
2020-11-11 15:15:49 +0100

    polishing

--- attila.js
+++ attila.js
@@ -1,8 +1,17 @@
 function create_doors() {
     for (let day = 1; day < 25; day++) {
 
+        let text_to_append
+
+        let date_of_door = new Date('November ' + day + ', 2020 0:0:0');
+        if (date_of_door < Date.now()) {
+            text_to_append = "secret"
+        } else {
+            text_to_append = "zfrüeh!"
+        }
+
         let front = $("<div></div>").addClass("front").text(day)
-        let back = $("<div></div>").addClass("back").text("fagotto")
+        let back = $("<div></div>").addClass("back").text(text_to_append)
         let card = $("<div></div>").addClass("card")
         card.append(front)
         card.append(back)
