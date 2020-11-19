d3dca8f80da1439f002b9fbf3d753bad0b663a06
Noah Zarro <noahzarro@gmail.com>
2020-11-19 20:40:20 +0100

    without content

--- attila.js
+++ attila.js
@@ -1,17 +1,42 @@
+function door_of_day(day, doors) {
+    doors.forEach(door => {
+        if (door["day"] == day) {
+            return day
+        }
+    });
+
+    return { "name": "Anonym", "media": "text", "day": 25, "link": "Gitter Gatter Gotter" }
+}
+
+
 function create_doors() {
     for (let day = 1; day < 25; day++) {
 
-        let text_to_append
+        let back = $("<div></div>").addClass("back")
 
         let date_of_door = new Date('November ' + day + ', 2020 0:0:0');
         if (date_of_door < Date.now()) {
-            text_to_append = "secret"
+            /*
+            cur_door = door_of_day(day)
+            if (cur_door["media"] == "video") {
+                back.append('<i class="fas fa-film"></i>').addClass("icon")
+
+            } else if (cur_door["media"] == "audio") {
+                back.append('<i class="fas fa-volume-up"></i>').addClass("icon")
+
+            } else if (cur_door["media"] == "pic") {
+                back.append('<i class="far fa-image"></i>').addClass("icon")
+
+            } else if (cur_door["media"] == "text") {
+                back.append('<i class="fas fa-file-alt"></i>').addClass("icon")
+
+            }
+            */
         } else {
-            text_to_append = "zfrüeh!"
+            back.text("zfrüeh!")
         }
 
         let front = $("<div></div>").addClass("front").text(day)
-        let back = $("<div></div>").addClass("back").text(text_to_append)
         let card = $("<div></div>").addClass("card")
         card.append(front)
         card.append(back)
@@ -27,4 +52,7 @@ function create_doors() {
 
 window.onload = function() {
     create_doors();
+    $.getJSON("https://n.ethz.ch/~zarron/attila/doors.json", function(doors) {
+        create_doors(doors);
+    })
 };
