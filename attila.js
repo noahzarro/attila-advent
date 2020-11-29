9a9392b1c42e56907e9daf44717cc1aeea2a9731
Noah Zarro <noahzarro@gmail.com>
2020-11-29 21:38:40 +0100

    done

--- attila.js
+++ attila.js
@@ -1,13 +1,24 @@
-function door_of_day(day, doors) {
+let eth_domain = "https://www.eth-lerngruppe.ch/advent/";
+let doors = []
+
+function door_of_day(day) {
+
+    let to_return = { "name": "Anonym", "media": "text", "day": 25, "link": "Gitter Gatter Gotter" }
+
     doors.forEach(door => {
         if (door["day"] == day) {
-            return day
+            to_return = door;
         }
     });
 
-    return { "name": "Anonym", "media": "text", "day": 25, "link": "Gitter Gatter Gotter" }
+    return to_return
 }
 
+function on_checked(checkboxElem, day) {
+    if (!checkboxElem.checked) {
+        show_modal(door_of_day(day));
+    }
+}
 
 function create_doors() {
     for (let day = 1; day < 25; day++) {
@@ -16,8 +27,9 @@ function create_doors() {
 
         let date_of_door = new Date('November ' + day + ', 2020 0:0:0');
         if (date_of_door < Date.now()) {
-            /*
+
             cur_door = door_of_day(day)
+
             if (cur_door["media"] == "video") {
                 back.append('<i class="fas fa-film"></i>').addClass("icon")
 
@@ -31,7 +43,7 @@ function create_doors() {
                 back.append('<i class="fas fa-file-alt"></i>').addClass("icon")
 
             }
-            */
+
         } else {
             back.text("zfrüeh!")
         }
@@ -40,7 +52,7 @@ function create_doors() {
         let card = $("<div></div>").addClass("card")
         card.append(front)
         card.append(back)
-        let input_elem = $('<input type="checkbox"></input>')
+        let input_elem = $('<input type="checkbox" onchange="on_checked(this, ' + day + ')"></input>')
         let label = $("<label></label>")
         label.append(input_elem)
         label.append(card)
@@ -50,11 +62,49 @@ function create_doors() {
     }
 }
 
+
+
+function show_modal(door) {
+    $("#modal").css("display", "block")
+
+    if (door["media"] == "pic") {
+        $("#img").css("display", "block")
+        $("#img").attr("src", eth_domain + door["link"]);
+    } else if (door["media"] == "video") {
+        $("#video").html('<source src="' + eth_domain + door["link"] + '" type="video/mp4"></source>');
+        $("#video").css("display", "block")
+        $("#video")[0].load();
+        //$("#video")[0].play();
+    }
+
+    $("#name").text(door["from"]);
+}
+
+function hide_modal() {
+
+    $("#img").css("display", "none")
+    $("#video").css("display", "none")
+    $("#video")[0].pause();
+
+    $("#modal").css("display", "none")
+}
+
 window.onload = function() {
-    create_doors();
-    /*
-    $.getJSON("https://n.ethz.ch/~zarron/attila/doors.json", function(doors) {
-        create_doors(doors);
+
+    $.getJSON(eth_domain + "doors.json", function(req_doors) {
+        doors = req_doors
+        create_doors();
     })
-    */
+
+
+    $("#close-btn").click(() => {
+        hide_modal()
+    })
 };
+
+
+window.addEventListener('click', function(e) {
+    if (!document.getElementById('modal-content').contains(e.target)) {
+        hide_modal()
+    }
+});
